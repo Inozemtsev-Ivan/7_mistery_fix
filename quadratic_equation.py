c858775536e254537d76444685141c55b645642f
inozemtsev-ivan <inozemtsev.il@gmail.com>
2017-11-21 18:00:12 +0300

    `get_roots()` function was simplified

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,13 +2,10 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
-    discriminant = b ** 2 - 4 * a * c
-    if discriminant < 0:
-        return None, None
-    else:    
+    try:
+        discriminant = b ** 2 - 4 * a * c
         root1 = (-b - sqrt(discriminant)) / (2 * a)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
-        if discriminant == 0:
-            return root1, None
-        else:
-            return root1, root2
+        return (root1, root2) if root1 != root2 else (root1, None)
+    except ValueError:
+        return None, None
